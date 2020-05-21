b56079a599f5c0c743ba518983d858fa86a1fa0c
Filipe Duarte <fmmendo@live.com>
2020-05-21 16:30:41 +0100

    added setup for oh-my-posh

---
+++ ohmyposh.ps1
@@ -0,0 +1,12 @@
+# Install/Set-Up oh-my-posh
+Install-Module posh-git -Scope CurrentUser
+Install-Module oh-my-posh -Scope CurrentUser
+Install-Module PSReadLine -Scope CurrentUser -Force
+
+Set-Prompt
+Set-Theme Agnoster
+
+if (!(Test-Path -Path $PROFILE )) { New-Item -Type File -Path $PROFILE -Force }
+
+Add-Content $profile "Import-Module posh-git"
+Add-Content $profile "Import-Module oh-my-posh"
