05769c9f7441b6e6268b25555e69064c7749464a
Filipe Duarte <fmmendo@live.com>
2020-05-28 23:05:13 +0100

    terminal settings

--- ohmyposh.ps1
+++ ohmyposh.ps1
@@ -11,3 +11,14 @@ if (!(Test-Path -Path $PROFILE )) { New-Item -Type File -Path $PROFILE -Force }
 Add-Content $profile "Import-Module posh-git"
 Add-Content $profile "Import-Module oh-my-posh"
 Add-Content $profile "Set-Theme Agnoster"
+
+# # Install Cascadia Code
+# # 1. Download Cascadia Code font from GitHub
+# $DLPath = "https://github.com/microsoft/cascadia-code/releases/download/v2005.15/CascadiaCode_2005.15.zip"
+# $DLFile = 'C:\Foo\Cascadia.TTF'
+# Invoke-WebRequest -Uri $DLPath -OutFile $DLFile
+
+# # 2. Now Install the Font 
+# $Font = New-Object -Com Shell.Application
+# $Destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
+# $Destination.CopyHere($DLFile,0x10)
